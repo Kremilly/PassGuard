a103295970a2d99702c3871f3efa89bb97fc4d68
Kremilly <contact@kremilly.com>
2024-07-05 16:53:13 -0300

    chore: update project metadata and improvements in password score

--- src/score.rs
+++ src/score.rs
@@ -6,6 +6,17 @@ pub struct PassScore {
 
 impl PassScore {
 
+    fn strength(&self, score: u8) -> String {
+        match score {
+            0 => "Very weak".to_string(),
+            1 => "Weak".to_string(),
+            2 => "Fair".to_string(),
+            3 => "Strong".to_string(),
+            4 => "Very strong".to_string(),
+            _ => "Unknown".to_string(),
+        }
+    }
+
     pub fn new(password: &str) -> Self {
         PassScore {
             password: password.to_string(),
@@ -14,11 +25,14 @@ impl PassScore {
 
     pub fn get(&mut self) {
         let result = zxcvbn(&self.password, &[]);
+
+        let score = result.score() as u8;
+        let strength = self.strength(score);
         let crack_time = result.crack_times().online_throttling_100_per_hour();
 
         println!("---------------------------------");
-        println!("Password strength: {}", result.score());
-        println!("Crack time: {}", crack_time);
+        println!("Password strength: {} (Score: {}/4)", strength, score);
+        println!("Crack time (Online with throttling (100 per hour)): {}", crack_time);
     }
 
 }
